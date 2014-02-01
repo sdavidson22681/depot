f733b2566aa948b49412f73c51e02512b8783f62
sdavidson22681 <sdavidson22681@pluto.dsu.edu>
2014-02-01 16:18:06 -0700

    Initial Add
    Adding files for CIS375_Assignment3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'cc6648ea9c226839ceb99779e6c8b36d16ce5f6ea11f570d11262d628d9eeee84496ab834820f983481d2cd8d228a995526026565c6b19ecc56340cf888670ef'
