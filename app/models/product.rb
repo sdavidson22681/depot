6090f59cc2e6dbef89a75849488631a2d8c6162f
sdavidson22681 <sdavidson22681@pluto.dsu.edu>
2014-02-18 18:06:56 -0700

    Catalog Display

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,3 +8,7 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 end
+
+def self.latest
+	Product.order(:updated_at).latest
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Depot::Application.routes.draw do
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,6 +54,6 @@ Depot::Application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #     resources :pro
+
 end
